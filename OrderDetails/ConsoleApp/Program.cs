58095eb6f399162df13c8c4e608168bd5ecaec99
Renu2811 <91143736+Renu2811@users.noreply.github.com>
2022-06-21 19:56:01 -0700

    Add files via upload

---
+++ OrderDetails/ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using OrderDetails;
+using OrderDetails.Entities;
+
+namespace ConsoleApp1
+{
+    public class program
+    {
+        public static void Main()
+        {
+            CRUDManager obj = new CRUDManager();
+
+            Console.WriteLine("Adding a new item");
+            obj.Insert(new Item { ItemName = "Phone", ItemRate = 19000, ItemQuantity = 1 });
+            obj.Insert(new Item { ItemName = "Watch", ItemRate = 2000, ItemQuantity =1 });
+            PrintAllitems();
+
+            //Console.WriteLine("Updating Items with customerID 2");
+            //obj.Update(2, new Employee { Name = "Modified Items", ItemRate = "Modified ItemRate",ItemQTY="Modified ItemQTY" });
+            //PrintAllEmployees();
+
+            //Console.WriteLine("Retrieving Item details for customerID 2");
+            //var employee = obj.GetitemById(2);
+            //Console.WriteLine($"Item Name of item ID 2 is {item.Name}");
+
+            //Console.WriteLine("Deleting Items details for customer 2");
+            //obj.Delete(2);
+            //PrintAllItem();
+
+            Console.ReadLine();
+        }
+
+        private static void PrintAllitems()
+        {
+            Console.WriteLine("Printing all items");
+            CRUDManager obj = new CRUDManager();
+            foreach (Item? item in obj.GetAllItems())
+            {
+                Console.WriteLine($"Items Name is {item.ItemName} and Rate is {item.ItemRate}");
+            }
+        }
+    }
+}
